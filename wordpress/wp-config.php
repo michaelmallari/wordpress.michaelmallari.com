cd3f6a732fdc8d3aa8f9f053d91dda80b2d8ff19
Michael Mallari <michael@michaelmallari>
2017-01-17 12:53:30 -0500

    Added MySQL credentials

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('MYSQL_DBNAME'));
+define('DB_NAME', $_ENV['MYSQL_DBNAME']);
 
 /** MySQL database username */
-define('DB_USER', getenv('MYSQL_USER'));
+define('DB_USER', $_ENV['MYSQL_USER']);
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
+define('DB_PASSWORD', $_ENV['MYSQL_PASSWORD']);
 
 /** MySQL hostname */
-define('DB_HOST', getenv('MYSQL_URI'));
+define('DB_HOST', $_ENV['MYSQL_URI']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
